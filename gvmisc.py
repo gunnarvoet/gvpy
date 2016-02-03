6245fd1bc717dafbfbde2bbdecb36b8f30ba8761
Gunnar Voet <gunnarvoet@gmail.com>
2016-02-03 09:26:20 -0800

    custom colormap

--- gvmisc.py
+++ gvmisc.py
@@ -32,3 +32,19 @@ def getshape(d):
     else:
         # Replace all non-dict values with None.
         return None
+
+def cmap_div(numcolors=11, name='custom_div_cmap',
+                    mincol='blue', midcol='white', maxcol='red'):
+    """ Create a custom diverging colormap with three colors
+    
+    Default is blue to white to red with 11 colors.  Colors can be specified
+    in any way understandable by matplotlib.colors.ColorConverter.to_rgb()
+    Adapted from http://pyhogs.github.io/colormap-examples.html
+    """
+
+    from matplotlib.colors import LinearSegmentedColormap 
+    
+    cmap = LinearSegmentedColormap.from_list(name=name, 
+                                             colors =[mincol, midcol, maxcol],
+                                             N=numcolors)
+    return cmap

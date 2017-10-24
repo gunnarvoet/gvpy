ad17b5515f97842b3e9306b3356b4d912361b843
Gunnar Voet <gvoet@ucsd.edu>
2017-10-24 16:39:24 -0700

    go back to gvimport

--- gvpy/__init__.py
+++ gvpy/__init__.py
@@ -2,11 +2,11 @@
 
 '''
 
-__all__ = ["colormaps", "figure", "matimport", "ocean", "misc"]
+__all__ = ["colormaps", "figure", "gvimport", "ocean", "misc"]
 
 __author__ = "Gunnar Voet"
 __email__ = "gvoet@ucsd.edu"
 __version__ = "0.1"
 
-from . import colormaps, figure, matimport, ocean, misc
+from . import colormaps, figure, gvimport, ocean, misc
 

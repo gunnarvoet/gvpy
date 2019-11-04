f3fb38398271ffd7b2b267f9bc677b1756894e14
Gunnar Voet <gvoet@ucsd.edu>
2019-11-04 11:46:06 -0800

    add mat2dataset to io

--- gvpy/io.py
+++ gvpy/io.py
@@ -7,6 +7,7 @@
 from __future__ import print_function, division
 import numpy as np
 import xarray as xr
+from  gvpy.gvimport import mtlb2datetime
 
 
 def read_sadcp(ncfile):
@@ -26,3 +27,68 @@ def read_sadcp(ncfile):
     sadcp = sadcp.transpose('z', 'time') 
     
     return sadcp
+
+
+def mat2dataset(m1):
+    k = m1.keys()
+
+    varsint = []
+    vars1d =  []
+    vars2d = []
+
+    for ki in k:
+        try:
+            tmp = m1[ki].shape
+            if len(tmp)==1:
+                vars1d.append(ki)
+            elif len(tmp)==2:
+                vars2d.append(ki)
+        except:
+            tmp = None
+            varsint.append(ki)
+    # let's find probable dimensions. usually we have p or z for depth
+    if 'z' in k:
+        jj = m1['z'].shape[0]
+    elif 'p' in k:
+        jj = m1['p'].shape[0]
+        
+    if 'lon' in k:
+        if len(m1['lon'].shape) == 1:
+            ii = m1['lon'].shape[0]
+    elif 'dnum' in k:
+        if len(m1['dnum'].shape) == 1:
+            ii = m1['dnum'].shape[0]
+
+    out = xr.Dataset(data_vars={'dummy': (['z', 'x'], np.ones((jj, ii)) * np.nan)})
+    # get 1d variables
+    for v in vars1d:
+        if m1[v].shape[0] == ii:
+            out[v] = (['x'], m1[v])
+        elif m1[v].shape[0] == jj:
+            out[v] = (['z'], m1[v])
+
+    # convert the usual suspects into variables
+    suspects = ['lon', 'lat', 'p', 'z', 'depth','dep']
+    for si in suspects:
+        if si in vars1d:
+            out.coords[si] = out[si]
+            
+    # convert time if possible
+    for si in ['datenum', 'dtnum', 'dnum']:
+        if si in vars1d and np.median(m1[si])>1e5:
+            out.coords['time'] = (['x'], mtlb2datetime(m1[si]))
+    
+    # get 2d variables
+    for v in vars2d:
+        if m1[v].shape[0]==ii and m1[v].shape[1]==jj:
+            out[v] = (['z', 'x'], m1[v])
+        elif m1[v].shape[0]==jj and m1[v].shape[1]==ii:
+            out[v] = (['z', 'x'], m1[v])
+            
+    # swap dim x for time if we have a time vector
+    if 'time' in out.coords:
+        out = out.swap_dims({'x': 'time'})
+            
+    # drop dummy
+    out = out.drop(['dummy'])
+    return out

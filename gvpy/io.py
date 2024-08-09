d6167553f349cb77ff79ffc120b66cb0ad76ed91
Gunnar Voet <gvoet@ucsd.edu>
2024-08-09 11:25:15 -0400

    chagne z to depth

--- gvpy/io.py
+++ gvpy/io.py
@@ -273,15 +273,18 @@ def read_sadcp(ncfile):
     mdepth = sadcp.depth.median(dim="time")
     sadcp = sadcp.drop("depth")
     # sadcp['depth'] = (['depth_cell'], mdepth)
-    sadcp = sadcp.rename_dims({"depth_cell": "z"})
-    sadcp.coords["z"] = (["z"], mdepth.data)
+    sadcp = sadcp.rename_dims({"depth_cell": "depth"})
+    sadcp.coords["depth"] = (["depth"], mdepth.data)
     # Fix some attributes
-    sadcp.z.attrs = dict(long_name="depth", units="m")
+    sadcp.depth.attrs = dict(long_name="depth", units="m")
     sadcp.u.attrs = dict(long_name="u", units="m/s")
     sadcp.v.attrs = dict(long_name="v", units="m")
     sadcp.time.attrs = dict(long_name="time", units="")
     # Transpose (re-order dimensions)
-    sadcp = sadcp.transpose("z", "time")
+    # sadcp = sadcp.transpose("depth", "time")
+    # Removing transposing as there may be more dimensions
+    # This may work:
+    # sadcp = sadcp.transpose("depth", "time", ...)
 
     return sadcp
 

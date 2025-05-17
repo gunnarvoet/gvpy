ffcbc7781d6e608e45aa8c9e978793378bd621a9
Gunnar Voet <gvoet@ucsd.edu>
2025-05-17 12:19:51 -0700

    update version nr and year

--- gvpy/__init__.py
+++ gvpy/__init__.py
@@ -41,7 +41,7 @@ which will provide several sub-modules, for example `gvpy.ocean`, as documented 
 
 ## License
 
-Copyright 2024 Gunnar Voet
+Copyright 2025 Gunnar Voet
 
 gvpy is free software: you can redistribute it and/or modify it under the terms
 of the GNU Lesser General Public License as published by the Free Software
